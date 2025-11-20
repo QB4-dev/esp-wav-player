fbde9f708f355d61dfe53223304f366027a002e7
kuba <qb4.dev@gmail.com>
2025-11-20 23:09:55 +0100

    tweaks for ESP32

--- include/esp_wav_player.h
+++ include/esp_wav_player.h
@@ -6,6 +6,7 @@
 #include "driver/i2s.h"
 #include "wav_object.h"
 
+#if CONFIG_IDF_TARGET_ESP8266
 #define ESP_WAV_PLAYER_DEFAULT_CONFIG() \
     {               \
     .i2s_num = I2S_NUM_0,                               \
@@ -26,6 +27,29 @@
     },                                                  \
     .queue_len = 4                                      \
 }
+#else
+#define ESP_WAV_PLAYER_DEFAULT_CONFIG() \
+    {               \
+    .i2s_num = I2S_NUM_0,                               \
+    .i2s_pin_config = {                                 \
+        .bck_io_num = GPIO_NUM_32,                       \
+        .ws_io_num = GPIO_NUM_25,                        \
+        .data_out_num = GPIO_NUM_33,                     \
+    },                                                  \
+    .base_cfg = {                                       \
+        .mode = I2S_MODE_MASTER | I2S_MODE_TX,         \
+        .sample_rate = 22050,                           \
+        .bits_per_sample = 16,                          \
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,   \           
+        .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_LSB, \
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,       \
+        .dma_buf_count = 4,                             \
+        .dma_buf_len = 256,                             \
+        .tx_desc_auto_clear = true                       \
+    },                                                  \
+    .queue_len = 4                                      \
+}
+#endif
 
 typedef void *esp_wav_player_t;
 typedef void (*esp_wav_player_cb_t)(esp_wav_player_t wav_player, void *arg);
